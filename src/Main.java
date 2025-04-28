bc8c927cf7228a82efd907cfc8b4d7318563f134
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-04-28 09:17:04 -0300

    Criando exemplo de array multidimensional

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Professor professor =
-                new Professor("João", 2000);
-        Aluno aluno1 = new Aluno(123, "Maria");
-        Aluno aluno2 = new Aluno(456, "Pedro");
-        Aluno aluno3 = new Aluno(789, "José");
-        Aluno alunos[] = {aluno1, aluno2, aluno3};
+        int array[][] = new int[3][];
+        array[0] = new int[5];
+        array[1] = new int[6];
+        array[2] = new int[3];
 
-        Turma turma = new Turma(professor, alunos);
+        int array2[][] = new int[3][4];
+
+        int array3[][] = {{3,4,5}, {4,5,7,8},{4,1,0,7}};
+
+        for(int i=0; i<array.length; i++){
+            for(int j=0; j<array[i].length;j++){
+                System.out.print(array[i][j]+" ");
+            }
+            System.out.println();
+        }
 
     }
 }
