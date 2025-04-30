927b99bed0b9cb6993e9407d62116d718a2a65db
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-04-30 07:27:35 -0300

    Exemplo de passagem por referência array

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int array[][] = new int[3][];
-        array[0] = new int[5];
-        array[1] = new int[6];
-        array[2] = new int[3];
+        int numeros[] = {4,5,6};
+        //Chamada por valor, mas passa a referência ao objeto
+        incrementarNumero(numeros);
+        for(int i: numeros) System.out.println(i);
+    }
 
-        int array2[][] = new int[3][4];
-
-        int array3[][] = {{3,4,5}, {4,5,7,8},{4,1,0,7}};
-
-        for(int i=0; i<array.length; i++){
-            for(int j=0; j<array[i].length;j++){
-                System.out.print(array[i][j]+" ");
-            }
-            System.out.println();
-        }
-
+    private static void incrementarNumero(int array[]) {
+        array[0]++;
     }
 }
