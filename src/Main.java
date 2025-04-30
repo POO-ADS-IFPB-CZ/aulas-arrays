37b9ca02775d6c3b21543c84880e88d35c571a85
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-04-30 08:33:31 -0300

    Resolvendo exercício de notas

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int numeros[] = {4,5,6};
-        //Chamada por valor, mas passa a referência ao objeto
-        incrementarNumero(numeros);
-        for(int i: numeros) System.out.println(i);
-    }
+        /*
+        Escreva um programa que lê as notas de X alunos.
+        Notas do tipo double, 0 até 10;
+        Apresentar as notas ordenadas.
+        Maior nota, Menor nota, média.
+         */
 
-    private static void incrementarNumero(int array[]) {
-        array[0]++;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe a quantidade de alunos: ");
+        int quantidade = scanner.nextInt();
+        double notas[] = new double[quantidade];
+        for(int i=0;i<quantidade;i++){
+            System.out.println("Informe a nota:");
+            notas[i] = scanner.nextDouble();
+        }
+        Arrays.sort(notas);
+        System.out.println(Arrays.toString(notas));
+        System.out.println("Menor: "+notas[0]);
+        System.out.println("Maior: "+notas[quantidade-1]);
+        double soma = 0;
+        for(double nota: notas) soma+=nota;
+        System.out.println("Média da turma: "+(soma/quantidade));
     }
 }
